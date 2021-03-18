f0a710bab510e1c651de81d33b4a2f1745422bf7
Everton Cruz <cruz.everton>
2021-03-18 14:05:36 -0300

    first commit

---
+++ src/app.js
@@ -0,0 +1,11 @@
+'use strict'
+
+const React = require('react')
+
+const App = React.createClass({
+  render: function () {
+    return <h1>Aplicação</h1>
+  }
+})
+
+export default App
---
+++ src/react-dom.js
@@ -0,0 +1,9 @@
+'use strict'
+
+function render () {
+  console.log('render')
+}
+
+export {render} // nomeado com short hand
+
+export default {render}
